6548b706f0db35e2b402da9c986b78426d67d5c0
John Siwicki <johnsiwicki@gmail.com>
2015-08-18 17:04:35 -0400

    cleanup css

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -20,10 +20,11 @@ myApp.controller('loginCtrl', function($scope, $http, $ionicModal, $timeout, $st
         })
       .success(function(data) {
            if (data.isLoggedIn === true) {
-            swal("Good job!", "You now loggedin!", "success");
-            $state.go("app.dashboard");
-            console.log(data.message);
-            localStorage.setItem('accessToken', JSON.stringify(data.accessToken));
+              swal("Good job!", "You now loggedin!", "success");
+              $state.go("app.dashboard");
+              console.log(data.message);
+              localStorage.setItem('accessToken', JSON.stringify(data.accessToken));
+              localStorage.setItem('company', JSON.stringify(data.company));
           }
           else {
               localStorage.clear();
@@ -85,43 +86,45 @@ $scope.urlForImage = function(imageName) {
     });
   };
 
- 
-   $scope.pushImage = function(type) {
 
+
+   $scope.upload = function(file) {
 
     $scope.imageData = {};
-    $scope.imageData.accessToken = '1147-cfaaf6384f7cab25782f919e5ef7c79a';
+    $scope.imageData.accessToken = '1147-90d28ebb3ffb493fdb11898e92782c85';
     $scope.imageData.company = '1015';
     $scope.imageData.group = 'billbyob';
 
     console.log( JSON.parse(localStorage.getItem('images')) );
     console.log(  $scope.images[0] );
- 
-   
-    var fileURL =   $scope.images[0]  ;
-    var fileLocal = JSON.parse(localStorage.getItem('images'));
-    
+
+    var fileURL =   $scope.images[0];   
     var uploadUrl = 'https://api.teambasementsystems.com/image/upload/';
 
-    var formData = new FormData();
-    formData.append('accessToken', $scope.imageData.accessToken);
-    formData.append('company', $scope.imageData.company);
-    formData.append('group',   $scope.imageData.group);
+    // var formData = new FormData();
+    // formData.append('accessToken', $scope.imageData.accessToken);
+    // formData.append('company', $scope.imageData.company);
+    // formData.append('group',   $scope.imageData.group);
 
-   Upload.upload({
 
-                file: localStorage.getItem('images'),
-                 url: uploadUrl,
-                 method: 'POST',
-                 headers: {'Content-Type': 'multipart/form-data'}, // only for html5 
-                 fileName: '1.jpg',
-                 fileFormDataName: 'file',
-                 data: {
-                    'accessToken':  $scope.imageData.accessToken,
-                    'company':  $scope.imageData.company,
-                    'group':  $scope.imageData.group
-                  }
-      });
+
+     Upload.upload({
+  
+                   file: fileURL,
+                   url: uploadUrl,
+                   method: 'POST',
+                   headers: {'Content-Type': 'multipart/form-data'},  
+                   fileName: '1.jpg',
+                   sendFieldsAs: 'form',
+                   fileFormDataName: 'file',
+                   data: {
+                      'accessToken':  $scope.imageData.accessToken,
+                      'company':  $scope.imageData.company,
+                      'group':  $scope.imageData.group,
+                    }
+        });
+
+
 
      
  };
